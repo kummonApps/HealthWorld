0bc1285a4bb32812195818b63db142cfd0f80a10
SrObis <sumogo.montero1@gmail.com>
2021-05-07 19:33:35 +0200

    ia acabada

---
+++ mensaje.js
@@ -0,0 +1,10 @@
+function mensaje() {
+
+    swal({
+      title: 'Mensaje enviado',
+      text: 'Mensaje de texto',
+      html: '<p>Pronto recibiras una respuesta.</p>',
+      type: 'success',
+      timer: 4000,
+    });
+  }
